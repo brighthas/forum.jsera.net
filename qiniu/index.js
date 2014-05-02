d0204bd898f6585653ff93dbcd8bc7fd34ca3b00
brighthas <brighthas@gmail.com>
2014-05-02 10:14:59 +0800

    update

--- qiniu/index.js
+++
@@ -1,6 +0,0 @@
-var qiniu = require("qiniu");
-
-qiniu.conf.ACCESS_KEY = 'DefBsvFPd0yTT_iafZ5hMq0uwo6g9GSi0hJXYAx6';
-qiniu.conf.SECRET_KEY = 'ZWtMpReZ3F9SNX2taQGJe0cKdxz-tRF5sGtinoVU';
-
-module.exports = qiniu;
